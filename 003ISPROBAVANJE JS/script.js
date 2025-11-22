0901a1a384897e9697ada9cefc88409959cae4c7
Tankred031 <tankred.kralj@gmail.com>
2025-11-22 23:44:09 +0100

    test js

---
+++ 003ISPROBAVANJE JS/script.js
@@ -0,0 +1,6 @@
+document.getElementById("colorBtn").addEventListener("click", function() {
+  const text = document.getElementById("message");
+
+  // Change from blue to green
+  text.style.color = "green";
+});
---
+++ script2.js
@@ -0,0 +1,4 @@
+document.getElementById("colorBtn").addEventListener("click", function() {
+  const text = document.getElementById("message");
+  text.style.color = "green";
+});
