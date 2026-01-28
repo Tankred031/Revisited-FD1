67d6fcefd3424246b1e1a2e266fccc6809167b45
Tankred031 <tankred.kralj@gmail.com>
2026-01-28 19:49:58 +0100

    vjezba08 pocetak

---
+++ 000POCETCI/Broj 6/skripta.js
@@ -0,0 +1,13 @@
+console.info('Hello world 5')
+console.table(JSON.parse('{"name":"John", "age":30, "car":null}'))
+
+console.log('O\'Neal')
+console.log("O'Neal")
+
+console.log(`
+Hello
+wolrd
+6
+`)
+
+document.getElementById('ispis').innerHTML='<a href="#">Poveznica</a>'
