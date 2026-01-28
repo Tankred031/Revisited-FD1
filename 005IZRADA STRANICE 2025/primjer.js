4583973a11b7a45a300507d346bb0035e751ca71
Tankred031 <tankred.kralj@gmail.com>
2026-01-28 22:22:41 +0100

    čišćenje beskorisnih stvari

--- 005IZRADA STRANICE 2025/primjer.js
+++
@@ -1,8 +0,0 @@
-const menu = document.getElementById('mymenu');
-const player = document.getElementById('player');
-
-menu.addEventListener('change', function() {
-  const selectedSong = this.value;
-  player.src = selectedSong;
-  player.play();
-});
