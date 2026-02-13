5b604c3f370f776093968c0f422a9ca7c72b16e8
Tankred031 <tankred.kralj@gmail.com>
2026-02-13 22:17:42 +0100

    web 4

--- 007_IZRADA_STRANICE-2026/webpage04/script.js
+++ 007_IZRADA_STRANICE-2026/webpage04/script.js
@@ -1,18 +1,29 @@
 let randomNumber = Math.floor (Math.random() * 100) + 1;
+let attempts = 10;
+
 function checkGuess () {
-    const inputElement = document.getElementById('guess').value;
+    attempts--;
+    const inputElement = document.getElementById('guess');
     const feedbackElement = document.getElementById('feedback');
     let guess = inputElement.value;
-    if (randomNumber == guess){
-        feedbackElement.innerHTML = "Good guess!";
-        feedbackElement.style.color = "green";
-    } else if (guess < randomNumber){
-        feedbackElement.innerHTML = "Too low!"
-        feedbackElement.style.color = "red"
-    } else {
-        feedbackElement.innerHTML = "Too high!"
-        feedbackElement.style.color = "red"
+    while (attempts > 0) {
+        if (randomNumber == guess){
+            feedbackElement.innerHTML = "Good guess!";
+            feedbackElement.style.color = "green";
+        } else if (guess < randomNumber){
+            feedbackElement.innerHTML = `Too low! Try again, ${attempts} attempts remaining`
+            feedbackElement.style.color = "red"
+        } else {
+            feedbackElement.innerHTML = `Too high! Try again, ${attempts} attempts remaining`
+            feedbackElement.style.color = "red"
+            break;
+        }
+    }
+    
+    if (attempts === 0 && guess != randomNumber) {
+        feedbackElement.innerHTML =
+        `Sorry, you're out of attempts! The correct number was ${randomNumber}.`;
+        feedbackElement.style.color = "red";
     }
 }
-
     
